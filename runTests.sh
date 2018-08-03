b1fd6a6fcebc7a30a7e5209c6f6b34f8973a5819
Łukasz Gandecki <lgandecki@css.edu>
2018-08-03 10:23:14 +0400

    use circleci reporter

--- runTests.sh
+++ runTests.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 export DISPLAY=:99.0
-if [$CIRCLE_TEST_REPORTS -ne ""]; then
-    export CIRCLE_TEST_REPORTS=.
-    export CUCUMBER_JSON_OUTPUT=$CIRCLE_TEST_REPORTS/cucumber/results.json
-else
-    export CUCUMBER_JSON_OUTPUT=./results.json
-fi
+
+CUCUMBER_JSON_OUTPUT=./test-results/chimp/results.json
 
 ./node_modules/.bin/chimpy features --jsonOutput=$CUCUMBER_JSON_OUTPUT
 
@@ -20,6 +16,6 @@ if [ $E2E_TESTS_RESULT -ne 0 ]; then
     E2E_TESTS_RESULT=$?
 fi
 
-cat $CUCUMBER_JSON_OUTPUT | ./node_modules/.bin/cucumber-junit > $CIRCLE_TEST_REPORTS/e2e-tests.xml
+# cat $CUCUMBER_JSON_OUTPUT | ./node_modules/.bin/cucumber-junit > $CIRCLE_TEST_REPORTS/e2e-tests.xml
 
 exit $E2E_TESTS_RESULT
